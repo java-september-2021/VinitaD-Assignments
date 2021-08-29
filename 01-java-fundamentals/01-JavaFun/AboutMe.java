764cea0a6fed3cd52e515d627716dea1db4e9fc0
vinita-daftari <daftarivinita@gmail.com>
2021-08-29 14:43:05 -0700

    javafun and alfred assignment

---
+++ 01-java-fundamentals/01-JavaFun/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("My name is Vinita Daftari");
+        System.out.println("my age is 32");
+        System.out.println("my hometown is Seattle");
+    }
+}
